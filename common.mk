63bfbfff07bbcf01666e4ea84e9b4c182051ebeb
Pranav Vashi <neobuddy89@gmail.com>
2022-06-12 02:28:07 +0530

    sm8750-common: overlay: Switch to crdroid doze
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- common.mk
+++ common.mk
@@ -158,6 +158,13 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml
 
+# Doze
+ifneq ($(TARGET_IS_TABLET),true)
+PRODUCT_PACKAGES += \
+    OPlusFrameworksResPhoneCommon \
+    OPlusSettingsResPhoneCommon
+endif
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm-service.clearkey
