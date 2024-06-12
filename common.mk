28eb42dd0a6790a27c1e3e8fa9a16d187f2ec146
Yumi Yukimura <me.cafebabe@gmail.com>
2024-06-12 22:41:36 +0800

    sm8750-common: Setup DeviceAsWebcam
    Change-Id: Icf8e10cd21c4eef9a66af624971ffe9c4d39d992
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- common.mk
+++ common.mk
@@ -330,6 +330,7 @@ DEVICE_PACKAGE_OVERLAYS += \
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_PACKAGES += \
+    DeviceAsWebcamResTarget \
     FrameworksResTargetCommon \
     NcmTetheringOverlay \
     OPlusFrameworksResCommon \
